309a62754d30691a6db64b3069de8f0558b34a38
Akshay Naik <akshay9_5@yahoo.co.in>
2018-10-15 21:16:05 +0530

    Updates

--- app/src/main/java/com/sidthak/remindme/adapters/AlarmsAdapter.java
+++ app/src/main/java/com/sidthak/remindme/adapters/AlarmsAdapter.java
@@ -1,8 +1,10 @@
 package com.sidthak.remindme.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.sidthak.remindme.R;
+import com.sidthak.remindme.activities.SetAlarmActivity;
 import com.sidthak.remindme.models.AlarmModel;
 import com.sidthak.remindme.models.WishModel;
 
@@ -26,15 +29,24 @@ public class AlarmsAdapter extends ArrayAdapter<AlarmModel> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View v = convertView;
-        AlarmModel wish = getItem(position);
+        AlarmModel alarm = getItem(position);
 
         if (v == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             v = inflater.inflate(R.layout.alarm_item, null);
         }
 
+        CardView abc = v.findViewById(R.id.card_view);
         TextView mClock = v.findViewById(R.id.alarm_time);
         TextView mSwitch = v.findViewById(R.id.alarm_switch);
+
+        abc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), SetAlarmActivity.class);
+                getContext().startActivity(i);
+            }
+        });
 
 //        TextView mTitle = v.findViewById(R.id.title);
 //        TextView mDescription = v.findViewById(R.id.description);
--- app/src/main/java/com/sidthak/remindme/models/AlarmModel.java
+++ app/src/main/java/com/sidthak/remindme/models/AlarmModel.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 public class AlarmModel {
     Date date;
+    String title;
     boolean activated;
-    
+
 }
